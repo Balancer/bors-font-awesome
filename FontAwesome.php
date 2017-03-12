0f2e4c3c018632d4a4a1273146f19c4fb0a5645d
Balancer <balancer@balancer.ru>
2017-03-12 05:15:39 +0300

    Initial commit.

---
+++ FontAwesome.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace B2;
+
+class FontAwesome
+{
+
+/*
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+*/
+
+	static function load()
+	{
+		if(empty(\bors::$bower_asset_packages['bower-asset/font-awesome']))
+			bors_use('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+		else
+			bors_use('/bower-asset/font-awesome/css/font-awesome.min.css');
+	}
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# bors/font-awesome
+
+[Font Awesome](http://fontawesome.io/) toolkit support for BORS© framework.
+
+```bash
+# To use CDN-version
+composer require bors/font-awesome
+
+# To use local asset version need also:
+composer require bower-asset/font-awesome
+```
+
+Local or CDN version detects automatically via `bower-asset/font-awesome` package install.
