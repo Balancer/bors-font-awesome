7ee10b9465585e4e5cffffeb0b2fd6a7cc4ab71c
Balancer <balancer@balancer.ru>
2018-03-10 23:49:00 +0300

    CDN fix and version bump.

--- FontAwesome.php
+++ FontAwesome.php
@@ -7,12 +7,13 @@ class FontAwesome
 
 /*
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
 */
 
 	static function load()
 	{
 		if(empty(\bors::$bower_asset_packages['bower-asset/font-awesome']))
-			bors_use('https://maxcdn.bootstrapcdn.com/font-awesome/5.0.7/css/font-awesome.min.css');
+			bors_use('https://use.fontawesome.com/releases/v5.0.8/css/all.css');
 		else
 			bors_use('/bower-asset/font-awesome/web-fonts-with-css/css/fontawesome-all.min.css');
 	}
